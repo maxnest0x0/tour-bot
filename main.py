fb502493010212159e0c8ac86bd957e030089790
Maxim Nesterov <udomix.udomix@gmail.com>
2023-10-13 17:15:28 +0000

    Bring the bot to life

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import telegram as tg
+import telegram.ext as ext
+
+from secret import token
+
+async def start(update: tg.Update, context: ext.ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hello World")
+
+if __name__ == "__main__":
+    app = ext.ApplicationBuilder().token(token).build()
+
+    app.add_handler(ext.CommandHandler("start", start))
+
+    app.run_polling()
---
+++ secret.py
@@ -0,0 +1,1 @@
+token = "6409765724:AAFx8V-167YJtdzDtfa4bmdJfVuI4Pbs2F4"
