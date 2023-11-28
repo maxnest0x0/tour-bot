68c3b2ea891c89521b36a003b0581d4b51e2a946
Maxim Nesterov <udomix.udomix@gmail.com>
2023-11-28 13:37:45 +0000

    Rewrite aviasales.ru auth in OOP

--- search/aviasales/browser.py
+++ search/aviasales/browser.py
@@ -1,13 +1,50 @@
 from selenium.webdriver import Chrome, ChromeOptions
+from typing import cast
 
-options = ChromeOptions()
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+class AviasalesBrowserAuthError(Exception):
+    pass
 
-driver = Chrome(options)
+class AviasalesBrowserAuth:
+    AVIASALES_URL = "https://aviasales.ru/"
+    CHROME_ARGS = [
+        "--headless=new",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+    ]
 
-driver.get("https://aviasales.ru/")
+    def __init__(self) -> None:
+        options = ChromeOptions()
+        for arg in self.CHROME_ARGS:
+            options.add_argument(arg)
 
-def get_token() -> str:
-    return driver.get_cookie("_awt")["value"]
+        self._driver = Chrome(options)
+        self._driver.get(self.AVIASALES_URL)
+
+    def is_driver_alive(self) -> bool:
+        try:
+            self._driver.current_url
+        except:
+            return False
+
+        return True
+
+    def _assert_driver_alive(self) -> None:
+        if not self.is_driver_alive():
+            raise AviasalesBrowserAuthError("browser is not running")
+
+    def get_token(self) -> str:
+        self._assert_driver_alive()
+
+        cookie = self._driver.get_cookie("_awt")
+        if cookie is None:
+            raise AviasalesBrowserAuthError("token cookie not found")
+
+        return cast(str, cookie["value"])
+
+    def refresh_page(self) -> None:
+        self._assert_driver_alive()
+        self._driver.refresh()
+
+    def quit(self) -> None:
+        self._assert_driver_alive()
+        self._driver.quit()
