dc05e41d5c2f555a96a07f26d664d9456cac7685
Maxim Nesterov <udomix.udomix@gmail.com>
2023-11-28 20:04:52 +0000

    Partially add a request for the search start API

--- search/aviasales/api.py
+++ search/aviasales/api.py
@@ -1,13 +1,17 @@
+from __future__ import annotations
+
 import requests
-from .browser import AviasalesBrowserAuth
 from typing import Any
+
+from .browser import AviasalesBrowserAuth
+from .data_types import SearchStartRequestData
 
 class AviasalesAPIError(Exception):
     pass
 
 class AviasalesAPI:
     AVIASALES_DOMAIN = "aviasales.ru"
-    START_ENDPOINT = f"https://tickets-api.{AVIASALES_DOMAIN}/search/v2/start"
+    SEARCH_START_ENDPOINT = f"https://tickets-api.{AVIASALES_DOMAIN}/search/v2/start"
 
     @staticmethod
     def raw_request(endpoint: str, token: str, body: Any) -> requests.Response:
@@ -42,3 +46,23 @@ class AviasalesAPI:
             return r.json()
         except requests.JSONDecodeError as error:
             raise AviasalesAPIError("invalid JSON") from error
+
+    def search_start(self, data: SearchStartRequestData) -> SearchAPI:
+        body = {
+            "search_params": data,
+            "marker": "direct",
+            "market_code": "ru",
+            "currency_code": "rub",
+            "languages": {
+                "ru": 1,
+            },
+        }
+
+        res = self.request(self.SEARCH_START_ENDPOINT, body)
+        return SearchAPI(self, res)
+
+class SearchAPI:
+    def __init__(self, api: AviasalesAPI, res: Any):
+        self._api = api
+        self.search_id = res["search_id"]
+        self.results_domain = res["results_url"]
---
+++ search/aviasales/data_types.py
@@ -0,0 +1,18 @@
+from typing import TypedDict, NotRequired, Sequence, Literal
+
+class Direction(TypedDict):
+    origin: str
+    destination: str
+    date: str
+
+class Passengers(TypedDict):
+    adults: int
+    children: NotRequired[int]
+    infants: NotRequired[int]
+
+TripClass = Literal["Y", "W", "C", "F"]
+
+class SearchStartRequestData(TypedDict):
+    directions: Sequence[Direction]
+    passengers: Passengers
+    trip_class: TripClass
