c93eed250fe1401b7aac1a5018976d57727ddc46
CrazysonRBTYG <147748703+CrazysonRBTYG@users.noreply.github.com>
2023-12-02 22:21:10 +0500

    Data vectorization

--- vectorization.py
+++ vectorization.py
@@ -1,13 +1,12 @@
 from sklearn.feature_extraction.text import CountVectorizer
-import sys
-import numpy
-numpy.set_printoptions(threshold=sys.maxsize)
 
 with open('Датасет.txt', 'r') as f:
     n = [i[:-2] for i in f]
 f.close()
 
-vectorizer = CountVectorizer(analyzer="word", ngram_range=(1, 4))
-X = vectorizer.fit_transform(n)
-vectorizer.get_feature_names_out()
-print(sum(list(arr).count(1) for arr in X.toarray()))
+
+myVectorizer = CountVectorizer(analyzer="word", ngram_range=(1, 4))
+data = myVectorizer.fit_transform(n)
+myVectorizer.get_feature_names_out()
+
+result = data.toarray()
