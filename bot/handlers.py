e60e3a6bb4843b169ca71f4ebace92cd39104c40
Max Nest <udomix.udomix@gmail.com>
2023-12-22 05:11:59 +0000

    Add type annotations for handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,21 +1,30 @@
+from __future__ import annotations
 import telegram as tg
 import telegram.ext as ext
+from typing import TYPE_CHECKING
 
 from .dialog import Dialog
 from .text import Text
 
+if TYPE_CHECKING:
+    from .app import Bot
+
 class Handlers:
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self._bot = bot
 
-    async def _send(self, update: tg.Update, text: str):
+    async def _send(self, update: tg.Update, text: str) -> None:
+        assert update.message is not None
         await update.message.reply_text(Text.welcome(), tg.constants.ParseMode.HTML, True)
 
-    async def start(self, update: tg.Update, context: ext.ContextTypes.DEFAULT_TYPE):
+    async def start(self, update: tg.Update, context: ext.ContextTypes.DEFAULT_TYPE) -> None:
         if update.message is not None:
-            await self._send(Text.welcome())
+            await self._send(update, Text.welcome())
 
-    async def message(self, update: tg.Update, context: ext.ContextTypes.DEFAULT_TYPE):
+    async def message(self, update: tg.Update, context: ext.ContextTypes.DEFAULT_TYPE) -> None:
+        if update.message is None or update.effective_chat is None:
+            return
+
         chat_id = update.effective_chat.id
 
         if chat_id in self._bot.dialogs:
@@ -31,4 +40,4 @@ class Handlers:
         if not dialog.is_busy():
             await dialog.process_input(update.message.text)
         else:
-            await self._send(Text.busy())
+            await self._send(update, Text.busy())
